08330c31d4087ec498ae73a55e415287463c891f
minht11 <justas543@gmail.com>
2021-07-30 21:53:37 +0300

    Add project files.

---
+++ GPS_data_visualizer_task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPS_data_visualizer_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
